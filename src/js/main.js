1ffa337a9dfe9fa090d9caabad11d957eee6de1c
Kvazar-harubdu <volodimrmihnyk@gmail.com>
2024-06-26 19:48:15 +0300

    add tabs block

--- src/js/main.js
+++ src/js/main.js
@@ -1,36 +1,63 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const tabs = document.querySelectorAll('.tabs-item');
 
-    if(tabs){
+    if (tabs) {
 
-    tabs.forEach(tab => {
-        const questionWrapper = tab.querySelector('.tabs-item__question__wrapper');
-        const content = tab.querySelector('.tabs-item__content');
-        const plusIcon = tab.querySelector('.plus');
-        const minusIcon = tab.querySelector('.minus');
+        tabs.forEach(tab => {
+            const questionWrapper = tab.querySelector('.tabs-item__question__wrapper');
+            const content = tab.querySelector('.tabs-item__content');
+            const plusIcon = tab.querySelector('.plus');
+            const minusIcon = tab.querySelector('.minus');
 
-        questionWrapper.addEventListener('click', function() {
-            if (!content.classList.contains('active')) {
-                // Hide all other tabs
-                tabs.forEach(item => {
-                    item.querySelector('.tabs-item__content').classList.remove('active');
-                    item.querySelector('.tabs-item__question__wrapper').classList.remove('active');
-                    item.querySelector('.plus').classList.remove('none');
-                    item.querySelector('.minus').classList.add('none');
+            questionWrapper.addEventListener('click', function () {
+                if (!content.classList.contains('active')) {
+                    // Hide all other tabs
+                    tabs.forEach(item => {
+                        item.querySelector('.tabs-item__content').classList.remove('active');
+                        item.querySelector('.tabs-item__question__wrapper').classList.remove('active');
+                        item.querySelector('.plus').classList.remove('none');
+                        item.querySelector('.minus').classList.add('none');
+                    });
+
+                    // Show the clicked tab
+                    content.classList.add('active');
+                    questionWrapper.classList.add('active');
+                    plusIcon.classList.add('none');
+                    minusIcon.classList.remove('none');
+                } else {
+                    // Hide the clicked tab
+                    content.classList.remove('active');
+                    questionWrapper.classList.remove('active');
+                    plusIcon.classList.remove('none');
+                    minusIcon.classList.add('none');
+                }
+            });
+        });
+    }
+    //tabs for informations
+    const tabInfo = document.querySelectorAll('[data-tab]');
+    
+    const tabsContent = document.querySelectorAll('[data-tab-content]');
+    if (tabInfo) {
+
+        tabInfo.forEach(function (item) {
+
+            item.addEventListener('click', function () {
+                let curentBtn = item;
+
+                tabInfo.forEach(function (item) {
+                    item.classList.remove('tab-active');
                 });
 
-                // Show the clicked tab
-                content.classList.add('active');
-                questionWrapper.classList.add('active');
-                plusIcon.classList.add('none');
-                minusIcon.classList.remove('none');
-            } else {
-                // Hide the clicked tab
-                content.classList.remove('active');
-                questionWrapper.classList.remove('active');
-                plusIcon.classList.remove('none');
-                minusIcon.classList.add('none');
-            }
+                curentBtn.classList.add('tab-active');
+
+                tabsContent.forEach(function (e) {
+                    e.classList.add('hidden');
+                });
+
+                const contentBox = document.querySelector('#' + this.dataset.tab);
+                contentBox.classList.remove('hidden');
+            });
         });
-    });
-}});
+    }
+});
